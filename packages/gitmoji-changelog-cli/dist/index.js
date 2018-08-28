b165f695f4c1a49ff16a5f03918545bfb36cf367
FRIN Yvonnick <frin.yvonnick@gmail.com>
2018-08-28 16:54:26 +0200

    :sparkles: Convert changelog to markdown

--- packages/gitmoji-changelog-cli/dist/index.js
+++ packages/gitmoji-changelog-cli/dist/index.js
@@ -12,12 +12,14 @@ var _fs2 = _interopRequireDefault(_fs);
 
 var _core = require('@gitmoji-changelog/core');
 
+var _markdown = require('@gitmoji-changelog/markdown');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 async function main() {
   try {
     const changes = await (0, _core.changelog)();
-    _fs2.default.writeFileSync('./CHANGELOG.json', changes);
+    _fs2.default.writeFileSync('./CHANGELOG.md', (0, _markdown.convert)(changes));
   } catch (e) {
     console.error('Cannot find a git repository in current path.');
   }
--- packages/gitmoji-changelog-cli/src/index.js
+++ packages/gitmoji-changelog-cli/src/index.js
@@ -1,11 +1,12 @@
 #! /usr/bin/env node
 import fs from 'fs'
 import { changelog } from '@gitmoji-changelog/core'
+import { convert } from '@gitmoji-changelog/markdown'
 
 export async function main() {
   try {
     const changes = await changelog()
-    fs.writeFileSync('./CHANGELOG.json', changes)
+    fs.writeFileSync('./CHANGELOG.md', convert(changes))
   } catch (e) {
     console.error('Cannot find a git repository in current path.')
   }
