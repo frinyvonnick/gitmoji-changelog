a4a12bb4f7133e7e5e40436da4c884f135abf03d
FRIN Yvonnick <frin.yvonnick@gmail.com>
2018-08-28 15:51:52 +0200

    :tada: Intialize markdown project

---
+++ packages/gitmoji-changelog-markdown/dist/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.format = format;
+function format() {
+  return 'Formatted';
+}
---
+++ packages/gitmoji-changelog-markdown/src/index.js
@@ -0,0 +1,3 @@
+export function format() {
+  return 'Formatted'
+}
