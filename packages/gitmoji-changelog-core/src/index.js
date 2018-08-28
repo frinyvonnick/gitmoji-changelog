195b59431dde83a4cff26be5c3ab362d97a9604e
FRIN Yvonnick <frin.yvonnick@gmail.com>
2018-08-28 10:48:18 +0200

    :sparkles: Implements parse commit function

--- packages/gitmoji-changelog-core/src/index.js
+++ packages/gitmoji-changelog-core/src/index.js
@@ -1,3 +1,26 @@
-export function transform() {
-  return 'Hello'
+import {
+  flow,
+  split,
+  compact,
+  map,
+} from 'lodash/fp'
+import splitLines from 'split-lines'
+
+export function parseCommit(commit) {
+  const [hash, date, subject, ...body] = splitLines(commit)
+
+  return {
+    hash,
+    date,
+    subject,
+    body: body.join('\n'),
+  }
+}
+
+export function parse(commits) {
+  return flow(
+    split('-hash-\n'),
+    compact,
+    map(parseCommit),
+  )(commits)
 }
--- packages/gitmoji-changelog-core/src/index.spec.js
+++ packages/gitmoji-changelog-core/src/index.spec.js
@@ -1,7 +1,34 @@
-import { transform } from './index.js'
+import { parse, parseCommit } from './index.js'
 
-describe('transform', () => {
-  it('should works', () => {
-    expect(transform()).toEqual('Hello')
+describe('commits parser', () => {
+  it('should parse a single commit', () => {
+    const commit = `c40ee8669ba7ea5151adc2942fa8a7fc98d9e23f
+2018-08-28T10:06:00+02:00
+:sparkles: Implements brand new feature
+Waouh this is awesome
+`
+    expect(parseCommit(commit)).toEqual({
+      hash: 'c40ee8669ba7ea5151adc2942fa8a7fc98d9e23f',
+      date: '2018-08-28T10:06:00+02:00',
+      subject: ':sparkles: Implements brand new feature',
+      body: 'Waouh this is awesome\n',
+    })
+  })
+
+  it('should multiple commits', () => {
+    const commits = `-hash-
+c40ee8669ba7ea5151adc2942fa8a7fc98d9e23f
+2018-08-28T10:06:00+02:00
+:sparkles: Implements brand new feature
+Waouh this is awesome
+`
+    expect(parse(commits)).toEqual([
+      {
+        hash: 'c40ee8669ba7ea5151adc2942fa8a7fc98d9e23f',
+        date: '2018-08-28T10:06:00+02:00',
+        subject: ':sparkles: Implements brand new feature',
+        body: 'Waouh this is awesome\n',
+      },
+    ])
   })
 })
