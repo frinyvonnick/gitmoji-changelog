44e401db08cc828fcc8c19b027350ac9d310773e
Benjamin Petetot <bpetetot@gmail.com>
2018-08-28 14:24:08 +0200

    :sparkles: implements changelog generation to json

--- packages/gitmoji-changelog-core/src/index.js
+++ packages/gitmoji-changelog-core/src/index.js
@@ -1,1 +1,21 @@
+import gitRawCommits from 'git-raw-commits'
+import through from 'through2'
+import concat from 'concat-stream'
+
+import { parseCommit } from './parser'
+
+const COMMIT_FORMAT = '%n%H%n%cI%n%s%n%b'
+
+export function changelog() {
+  return new Promise((resolve) => {
+    gitRawCommits({
+      format: COMMIT_FORMAT,
+    }).pipe(through.obj((data, enc, next) => {
+      next(null, parseCommit(data.toString()))
+    })).pipe(concat(data => {
+      resolve(JSON.stringify(data))
+    }))
+  })
+}
+
 export * from './parser'
---
+++ packages/gitmoji-changelog-core/src/index.spec.js
@@ -0,0 +1,36 @@
+/* eslint-disable global-require */
+import gitRawCommits from 'git-raw-commits'
+
+import { changelog } from './index'
+
+describe('changelog', () => {
+  beforeAll(() => {
+    gitRawCommits.mockImplementation(() => {
+      const stream = require('stream')
+      const readable = new stream.Readable()
+      readable.push(`c40ee8669ba7ea5151adc2942fa8a7fc98d9e23f
+2018-08-28T10:06:00+02:00
+:sparkles: Upgrade brand new feature
+Waouh this is awesome 2
+`)
+      readable.push(null)
+      readable.emit('close')
+      return readable
+    })
+  })
+
+  it('should generate changelog in json format', async () => {
+    const result = await changelog()
+
+    expect(result).toEqual(JSON.stringify([
+      {
+        hash: 'c40ee8669ba7ea5151adc2942fa8a7fc98d9e23f',
+        date: '2018-08-28T10:06:00+02:00',
+        subject: ':sparkles: Upgrade brand new feature',
+        body: 'Waouh this is awesome 2',
+      },
+    ]))
+  })
+})
+
+jest.mock('git-raw-commits');
