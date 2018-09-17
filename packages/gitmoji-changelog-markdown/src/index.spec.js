2d783e44e7d4b84a993237e27b95a5d27532426b
FRIN Yvonnick <frin.yvonnick@gmail.com>
2018-09-17 14:21:58 +0200

    :sparkles: Generate changelog for the next release (#8)

--- packages/gitmoji-changelog-markdown/src/index.spec.js
+++ packages/gitmoji-changelog-markdown/src/index.spec.js
@@ -2,18 +2,38 @@ const { convert } = require('./index')
 
 describe('Markdown converter', () => {
   it('should convert json changelog into markdown', () => {
-    const json = [
-      {
-        hash: 'c40ee8669ba7ea5151adc2942fa8a7fc98d9e23f',
-        date: '2018-08-28T10:06:00+02:00',
-        subject: ':sparkles: Upgrade brand new feature',
-        body: 'Waouh this is awesome 2',
+    const json = {
+      next: {
+        commits: [
+          {
+            hash: 'c40ee8669ba7ea5151adc2942fa8a7fc98d9e23c',
+            date: '2018-08-28T10:07:00+02:00',
+            subject: ':sparkles: Upgrade brand new feature',
+            body: 'Waouh this is awesome 3',
+          },
+        ],
       },
-    ]
+      'v1.0.0': {
+        commits: [
+          {
+            hash: 'c40ee8669ba7ea5151adc2942fa8a7fc98d9e23f',
+            date: '2018-08-28T10:06:00+02:00',
+            subject: ':sparkles: Upgrade brand new feature',
+            body: 'Waouh this is awesome 2',
+          },
+        ],
+      },
+    }
 
-    expect(convert(JSON.stringify(json))).toEqual(`# Changelog
+    expect(convert(json)).toEqual(`# Changelog
 
-- ${json[0].subject} (${json[0].hash})
+## next
+
+- :sparkles: Upgrade brand new feature (c40ee8669ba7ea5151adc2942fa8a7fc98d9e23c)
+
+## v1.0.0
+
+- :sparkles: Upgrade brand new feature (c40ee8669ba7ea5151adc2942fa8a7fc98d9e23f)
 `)
   })
 })
