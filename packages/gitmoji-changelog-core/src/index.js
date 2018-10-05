cba6ffa2a062192ce6045b837cba1de4e7de6d41
FRIN Yvonnick <frin.yvonnick@gmail.com>
2018-10-05 15:39:56 +0200

    :sparkles: Sort commits by emoji and date

--- packages/gitmoji-changelog-core/src/index.js
+++ packages/gitmoji-changelog-core/src/index.js
@@ -32,7 +32,13 @@ function makeGroups(commits) {
     .map(({ group, label }) => ({
       group,
       label,
-      commits: commits.filter(commit => commit.group === group),
+      commits: commits
+        .filter(commit => commit.group === group)
+        .sort((first, second) => {
+          const emojiCriteria = first.emoji.localeCompare(second.emoji)
+          if (emojiCriteria !== 0) return emojiCriteria
+          return first.date.localeCompare(second.date)
+        }),
     }))
     .filter(group => group.commits.length)
 }
--- packages/gitmoji-changelog-core/src/index.spec.js
+++ packages/gitmoji-changelog-core/src/index.spec.js
@@ -5,7 +5,7 @@ const gitSemverTags = require('git-semver-tags')
 const { generateChangelog } = require('./index')
 
 const sparklesCommit = {
-  hash: 'c40ee8669ba7ea5151adc2942fa8a7fc98d9e23f',
+  hash: 'c40ee8669ba7ea5151adc2942fa8a7fc98d9e23a',
   date: '2018-08-28T10:06:00+02:00',
   subject: ':sparkles: Upgrade brand new feature',
   body: 'Waouh this is awesome 2',
@@ -13,9 +13,30 @@ const sparklesCommit = {
 
 const recycleCommit = {
   hash: 'c40ee8669ba7ea5151adc2942fa8a7fc98d9e23c',
-  date: '2018-08-28T10:07:00+02:00',
+  date: '2018-08-01T10:07:00+02:00',
   subject: ':recycle: Upgrade brand new feature',
   body: 'Waouh this is awesome 3',
+}
+
+const secondRecycleCommit = {
+  hash: 'c40ee8669ba7ea5151adc2942fa8a7fc98d9e23d',
+  date: '2018-08-30T10:07:00+02:00',
+  subject: ':recycle: Upgrade another brand new feature',
+  body: 'Waouh this is awesome 4',
+}
+
+const lipstickCommit = {
+  hash: 'c40ee8669ba7ea5151adc2942fa8a7fc98d9e23e',
+  date: '2018-08-10T10:07:00+02:00',
+  subject: ':lipstick: Change graphics for a feature',
+  body: 'Waouh this is awesome 5',
+}
+
+const secondLipstickCommit = {
+  hash: 'c40ee8669ba7ea5151adc2942fa8a7fc98d9e23f',
+  date: '2018-08-18T10:07:00+02:00',
+  subject: ':lipstick: Change more graphics for a feature',
+  body: 'Waouh this is awesome 6',
 }
 
 describe('changelog', () => {
@@ -35,16 +56,16 @@ describe('changelog', () => {
           {
             group: 'changed',
             label: 'Changed',
-            commits: [
+            commits: expect.arrayContaining([
               expect.objectContaining(recycleCommit),
-            ],
+            ]),
           },
         ],
       },
     ])
   })
 
-  it('should generate changelog in json format for all tags', async () => {
+  it.only('should generate changelog in json format for all tags', async () => {
     gitSemverTags.mockImplementation((cb) => cb(null, ['v1.0.0']))
 
     const { changes } = await generateChangelog()
@@ -70,7 +91,10 @@ describe('changelog', () => {
             group: 'changed',
             label: 'Changed',
             commits: [
+              expect.objectContaining(lipstickCommit),
+              expect.objectContaining(secondLipstickCommit),
               expect.objectContaining(recycleCommit),
+              expect.objectContaining(secondRecycleCommit),
             ],
           },
         ],
@@ -82,33 +106,26 @@ describe('changelog', () => {
 jest.mock('git-raw-commits')
 jest.mock('git-semver-tags')
 
+function mockCommit(commits) {
+  gitRawCommits.mockImplementationOnce(() => {
+    const stream = require('stream')
+    const readable = new stream.Readable()
+    commits.forEach(commit => {
+      const {
+        hash,
+        date,
+        subject,
+        body,
+      } = commit
+      readable.push(`\n${hash}\n${date}\n${subject}\n${body}\n`)
+    })
+    readable.push(null)
+    readable.emit('close')
+    return readable
+  })
+}
+
 function mockCommits() {
-  gitRawCommits.mockImplementationOnce(() => {
-    const stream = require('stream')
-    const readable = new stream.Readable()
-    const {
-      hash,
-      date,
-      subject,
-      body,
-    } = recycleCommit
-    readable.push(`\n${hash}\n${date}\n${subject}\n${body}\n`)
-    readable.push(null)
-    readable.emit('close')
-    return readable
-  })
-  gitRawCommits.mockImplementationOnce(() => {
-    const stream = require('stream')
-    const readable = new stream.Readable()
-    const {
-      hash,
-      date,
-      subject,
-      body,
-    } = sparklesCommit
-    readable.push(`\n${hash}\n${date}\n${subject}\n${body}\n`)
-    readable.push(null)
-    readable.emit('close')
-    return readable
-  })
+  mockCommit([sparklesCommit])
+  mockCommit([recycleCommit, secondRecycleCommit, lipstickCommit, secondLipstickCommit])
 }
