0b296992ae80ace0ce6eeb8b1faa9d654964aea5
FRIN Yvonnick <frin.yvonnick@gmail.com>
2019-06-21 16:56:21 +0200

    :white_check_mark: Add e2e tests for available workflows

--- packages/gitmoji-changelog-cli/src/cli.e2e.spec.js
+++ packages/gitmoji-changelog-cli/src/cli.e2e.spec.js
@@ -3,15 +3,14 @@ const fs = require('fs')
 const { removeSync } = require('fs-extra')
 const path = require('path')
 const simpleGit = require('simple-git/promise')
-const util = require('util')
-const execFile = util.promisify(require('child_process').execFile)
+const childProcess = require('child_process')
 
 expect.extend({
   includes(str, items) {
     const pass = items.every(item => str.includes(item))
     return {
       pass,
-      message: () => pass ? '' : '',
+      message: () => pass ? 'It passes' : `Expected ${str} to includes ${items.join(', ')}`,
     }
   },
 })
@@ -20,7 +19,7 @@ describe('generate changelog', () => {
   let testDir
   let repo
 
-  beforeAll(async () => {
+  beforeEach(async () => {
     testDir = path.join(os.tmpdir(), 'gitmoji-changelog')
     if (fs.existsSync(testDir)) removeSync(testDir)
     fs.mkdirSync(testDir)
@@ -32,33 +31,113 @@ describe('generate changelog', () => {
     )
   })
 
-  afterAll(() => {
+  afterEach(() => {
     removeSync(testDir)
   })
 
-  it('should generate changelog even if user doesn\'t call cli at each tag', async () => {
-    await newVersion('1.0.0')
-    await gitmojiChangelog()
+  describe('init', () => {
+    it("should get a 1.0.0 version while initializing changelog by calling cli without arguments and having package.json's version set to 1.0.0", async () => {
+      await makeChanges('file1')
+      await commit(':sparkles: Add some file')
+      await bumpVersion('1.0.0')
+      gitmojiChangelog()
+      await commit(':bookmark: Version 1.0.0')
 
-    expect(getChangelog()).toMatch(/1.0.0/)
+      expect(getChangelog()).includes(['1.0.0'])
+    })
 
-    await newVersion('1.0.1')
-    await newVersion('1.0.2')
-    await gitmojiChangelog()
+    it("should get a 1.0.0 version while initializing changelog by calling cli with 1.0.0 and having package.json's version set to 0.0.1", async () => {
+      await makeChanges('file1')
+      await commit(':sparkles: Add some file')
+      gitmojiChangelog('1.0.0')
+      await commit(':bookmark: Version 1.0.0')
 
-    expect(getChangelog()).includes(['1.0.0', '1.0.1', '1.0.2'])
+      expect(getChangelog()).includes(['1.0.0'])
+    })
   })
 
-  async function newVersion(version) {
-    fs.writeFileSync(path.join(testDir, version))
-    bumpVersion(version)
+  describe('update', () => {
+    it("should get two versions 1.0.0 and next while updating changelog by calling cli without arguments and having package.json's version set to 1.0.0", async () => {
+      await makeChanges('file1')
+      await commit(':sparkles: Add some file')
+      await bumpVersion('1.0.0')
+      gitmojiChangelog()
+      await commit(':bookmark: Version 1.0.0')
+      await tag('1.0.0')
+
+      await makeChanges('file2')
+      await commit(':sparkles: Add another file')
+      gitmojiChangelog()
+
+      expect(getChangelog()).includes(['1.0.0', 'next'])
+    })
+    it("should get two versions 1.0.0 and 2.0.0 while updating changelog by calling cli without arguments and having package.json's version set to 2.0.0", async () => {
+      await makeChanges('file1')
+      await commit(':sparkles: Add some file')
+      await bumpVersion('1.0.0')
+      gitmojiChangelog()
+      await commit(':bookmark: Version 1.0.0')
+      await tag('1.0.0')
+
+      await makeChanges('file2')
+      await commit(':sparkles: Add another file')
+      await bumpVersion('2.0.0')
+      gitmojiChangelog()
+
+      expect(getChangelog()).includes(['1.0.0', '2.0.0'])
+    })
+    it("should get two versions 1.0.0 and 2.0.0 while updating changelog by calling cli with 2.0.0 and having package.json's version set to 1.0.0", async () => {
+      await makeChanges('file1')
+      await commit(':sparkles: Add some file')
+      await bumpVersion('1.0.0')
+      gitmojiChangelog()
+      await commit(':bookmark: Version 1.0.0')
+      await tag('1.0.0')
+
+      await makeChanges('file2')
+      await commit(':sparkles: Add another file')
+      gitmojiChangelog('2.0.0')
+
+      expect(getChangelog()).includes(['1.0.0', '2.0.0'])
+    })
+    it('should get three versions 1.0.0, 2.0.0, 3.0.0 while updating changelog by calling cli without arguments and skipping two tags creation 2.0.0 and 3.0.0', async () => {
+      await makeChanges('file1')
+      await commit(':sparkles: Add some file')
+      await bumpVersion('1.0.0')
+      gitmojiChangelog()
+      await commit(':bookmark: Version 1.0.0')
+      await tag('1.0.0')
+
+      await makeChanges('file2')
+      await commit(':sparkles: Add another file')
+      await bumpVersion('2.0.0')
+      await commit(':bookmark: Version 2.0.0')
+      await tag('2.0.0')
+
+      await makeChanges('file3')
+      await commit(':sparkles: Add a third file')
+      await bumpVersion('3.0.0')
+      gitmojiChangelog()
+      await commit(':bookmark: Version 3.0.0')
+      await tag('3.0.0')
+    })
+  })
+
+  async function makeChanges(fileName) {
+    fs.writeFileSync(path.join(testDir, fileName))
+  }
+
+  async function commit(message) {
     await repo.add('.')
-    await repo.commit(`Commit ${version}`)
+    await repo.commit(message)
+  }
+
+  async function tag(version) {
     await repo.addTag(`v${version}`)
   }
 
-  function gitmojiChangelog() {
-    return execFile('node', [path.join(__dirname, 'index.js')], { cwd: testDir })
+  function gitmojiChangelog(args = []) {
+    return childProcess.execFileSync('node', [path.join(__dirname, 'index.js'), ...args], { cwd: testDir })
   }
 
   function getChangelog() {
