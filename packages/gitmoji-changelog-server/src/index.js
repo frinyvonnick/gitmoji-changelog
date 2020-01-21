63b82f7b88d2ce1b279aa5cff4f03e5dcbae243b
FRIN Yvonnick <frin.yvonnick@gmail.com>
2020-01-21 22:08:07 +0100

    :rotating_light: Fix lint warnings

--- packages/gitmoji-changelog-server/src/index.js
+++ packages/gitmoji-changelog-server/src/index.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 const childProcess = require('child_process')
 const { removeSync } = require('fs-extra')
 const simpleGit = require('simple-git/promise')
-const cors = require('cors') 
+const cors = require('cors')
 
 const app = express()
 const port = process.env.PORT
@@ -13,8 +13,8 @@ const port = process.env.PORT
 app.use(cors())
 
 app.get('/', async (req, res) => {
-  const { repository } = req.query 
-  const tempDir = path.join(os.tmpdir(), 'gitmoji-changelog') 
+  const { repository } = req.query
+  const tempDir = path.join(os.tmpdir(), 'gitmoji-changelog')
   if (fs.existsSync(tempDir)) removeSync(tempDir)
   fs.mkdirSync(tempDir)
 
@@ -25,7 +25,7 @@ app.get('/', async (req, res) => {
   const cwd = path.join(tempDir, repository.split('/').pop().slice(0, -4))
   childProcess.execSync(path.join(__dirname, '..', 'node_modules/.bin/gitmoji-changelog'), { cwd })
 
-  const output = fs.readFileSync(path.join(cwd, 'CHANGELOG.md')) 
+  const output = fs.readFileSync(path.join(cwd, 'CHANGELOG.md'))
   removeSync(tempDir)
 
   res.send(output.toString('utf8'))
